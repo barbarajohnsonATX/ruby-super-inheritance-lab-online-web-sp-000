3def362a680c6ff073f48f441d1dfc64ba386f9c
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-15 17:04:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,10 @@
 class Student 
   
   def hello
-<<<<<<< HEAD
+ 
     puts "Hey there! I'm so excited to learn stuff."
-=======
-    puts "Hello"
->>>>>>> 7acb2d592e01e46d6e0f8d2f3d9a39fe88edb851
-  end 
+ 
+   end 
   
   def raise_hand 
     puts "Pick me!"
